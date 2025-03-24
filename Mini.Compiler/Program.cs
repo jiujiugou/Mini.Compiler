4f13bd2e62edb32ff3f3ad983ee09f90db009b5f
jiujiugou <jiujiugo_123@qq.com>
2025-03-24 22:19:50 +0800

    添加项目文件。

---
+++ Mini.Compiler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini.Compiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
